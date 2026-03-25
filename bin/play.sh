d9f86929f42106f883148385e0671613e409c986
Amber Renfroe <Amber.renfroe@gmail.com>
2026-03-25 02:46:25 +0000

    created play.sh

---
+++ bin/play.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# --- TALES FROM THE SCRIPT ---
+
+# The Script Keeper's Pun-Protocol
+script_keeper() {
+    echo -e "\e[1;35m[The Script Keeper]:\e[0m $1"
+    sleep 1.2
+}
+
+clear
+echo "----------------------------------------------------"
+echo "        TALES FROM THE SCRIPT               "
+echo "----------------------------------------------------"
+echo ""
+
+script_keeper "Greetings, Boils and Ghouls! Heh-heh-heh!"
+script_keeper "I see you've 'log-ged' in for a little... terminal illness!"
+
+# Save your class selection here next...
